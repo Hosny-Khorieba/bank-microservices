11e427c071e1e5bf391d69ecd9b096409fadfd3b
hosny.emad <hemad@ejada.com>
2023-09-17 13:39:42 +0300

    resilience4j

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -25,6 +25,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 /**
  * @author Eazy Bytes
  *
@@ -66,6 +70,8 @@ public class AccountsController {
 	}
 	
 	@PostMapping("/myCustomerDetails")
+//	@CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallback")
+	@Retry(name = "retryForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallback")
 	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans = loansFeignClient.getListOfLoans(customer);
@@ -78,6 +84,28 @@ public class AccountsController {
 		
 		return customerDetails;
 
+	}
+	
+	public CustomerDetails myCustomerDetailsFallback(Customer customer, Throwable t) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getListOfLoans(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		
+		return customerDetails;
+
+	}
+	
+	@GetMapping("/hello")
+	@RateLimiter(name = "helloRateLimiter", fallbackMethod = "sayHelloFallback")
+	public String sayHello() {
+		return "Hello to the bank";
+	}
+	
+	public String sayHelloFallback(Throwable t) {
+		return "Hello to the bank fallback";
 	}
 
 }
